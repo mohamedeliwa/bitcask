5e13374429e3ecfcab4da1bad705e505f8371f24
mohamedeliwa <mohamed.av11@gmail.com>
2024-11-24 21:58:24 +0200

    each store now is directory and contains log and index files

--- src/infrastructure/repositories/disk_store_repo.rs
+++ src/infrastructure/repositories/disk_store_repo.rs
@@ -1,4 +1,4 @@
-use std::{fs, sync::Arc};
+use std::{fs, path::PathBuf, sync::Arc};
 
 use crate::{
     domain::{entities::store::Store, repositories::store_repo::StoreRepo},
@@ -8,6 +8,8 @@ use crate::{
 pub struct DiskStoreRepo {
     /// path of the directory in which stores are created
     path: String,
+    log_file_name: String,
+    index_file_name: String,
 }
 
 impl DiskStoreRepo {
@@ -17,6 +19,8 @@ impl DiskStoreRepo {
 
         DiskStoreRepo {
             path: path.to_string(),
+            log_file_name: "log".into(),
+            index_file_name: "index.json".into(),
         }
     }
 }
@@ -28,9 +32,25 @@ impl DiskStoreRepo {
 }
 
 impl StoreRepo for Arc<DiskStoreRepo> {
+    /// creates the store directory structure
+    /// each store consits of a directory and two files
+    /// and one for storing the data called log
+    /// and one for storing the hash index called index
     fn create(&self, store: &NewStore) -> Result<(), String> {
-        let path = format!("{}/{}", self.path, store.name);
-        match fs::File::create_new(&path) {
+        let mut path = PathBuf::from(&self.path);
+        path.push(&store.id);
+
+        // creating the directory container for the new store
+        fs::create_dir_all(&path).expect("couldn't create store directory");
+
+        let log_file = path.join(&self.log_file_name);
+        let index_file = path.join(&self.index_file_name);
+
+        match fs::File::create_new(&log_file) {
+            Err(e) => return Err(e.to_string()),
+            _ => {}
+        };
+        match fs::File::create_new(&index_file) {
             Ok(_) => Ok(()),
             Err(e) => Err(e.to_string()),
         }
