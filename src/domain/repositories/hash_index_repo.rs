5d9ddad0f0bbe869fcf17e31b536d55c865bc1c1
mohamedeliwa <mohamed.av11@gmail.com>
2024-11-27 23:25:04 +0200

    created a function to persist the hash index of a store

--- src/domain/repositories/hash_index_repo.rs
+++ src/domain/repositories/hash_index_repo.rs
@@ -5,4 +5,6 @@ pub trait HashIndexRepo {
     fn set(&self, key: &str, offset: Offset) -> Result<(), String>;
     /// gets the offset of a record in the databse file by its key
     fn get(&self, key: &str) -> Option<Offset>;
+    /// persists the current hash index for a store on disk
+    fn persist(&self, store: &str, index_name: &str) -> Result<(), String>;
 }
--- src/infrastructure/repositories/mem_hash_index_repo.rs
+++ src/infrastructure/repositories/mem_hash_index_repo.rs
@@ -1,5 +1,8 @@
 use std::{
     collections::HashMap,
+    fs::OpenOptions,
+    io::Write,
+    path::PathBuf,
     sync::{Arc, Mutex},
 };
 
@@ -8,22 +11,55 @@ use crate::domain::{
     repositories::hash_index_repo::HashIndexRepo,
 };
 
-pub struct MemHashIndexRepo(Mutex<HashIndex>);
+pub struct MemHashIndexRepo {
+    path: String,
+    index: Mutex<HashIndex>,
+}
 
 impl MemHashIndexRepo {
-    pub fn new() -> Self {
-        MemHashIndexRepo(Mutex::new(HashIndex {
-            index: HashMap::new(),
-        }))
+    pub fn new(path: &str) -> Self {
+        MemHashIndexRepo {
+            index: Mutex::new(HashIndex {
+                index: HashMap::new(),
+            }),
+            path: path.into(),
+        }
     }
 }
 
 impl HashIndexRepo for Arc<MemHashIndexRepo> {
     fn get(&self, key: &str) -> Option<Offset> {
-        self.0.lock().unwrap().index.get(key).copied()
+        self.index.lock().unwrap().index.get(key).copied()
     }
     fn set(&self, key: &str, offset: Offset) -> Result<(), String> {
-        self.0.lock().unwrap().index.insert(key.into(), offset);
+        self.index.lock().unwrap().index.insert(key.into(), offset);
+        Ok(())
+    }
+
+    /// persists the in-memory index of a store to disk.
+    ///
+    /// # Arguments
+    /// * `store` - The store id.
+    /// * `index_name` - The name for the newly created index file.
+    ///
+    fn persist(&self, store: &str, index_name: &str) -> Result<(), String> {
+        let index_path = format!("{store}/{index_name}.index");
+        let index_path = PathBuf::from(&self.path).join(&index_path);
+        // append the timestamp to the meta file
+        let mut index_file = OpenOptions::new()
+            .append(true)
+            .open(index_path)
+            .map_err(|e| e.to_string())?;
+        // empty the cache
+        let in_mem_index: Vec<(String, Offset)> = self.index.lock().unwrap().index.drain().collect();
+
+        // write the cached value to it
+        for (key, value) in in_mem_index {
+            let record = format!("{key},{}\n", value.0);
+            index_file
+                .write_all(record.as_bytes())
+                .map_err(|e| e.to_string())?;
+        }
         Ok(())
     }
 }
